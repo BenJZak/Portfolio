eb2f98c96624dc440698fb54d47847bb8bae8a6d
benzakielarz <bjzakielarz@loyola.edu>
2024-09-16 18:23:26 -0400

    removed unecessary content such as unused images, files, imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
-import Sidenav from './components/Sidenav'
 import Main from './components/Main'
 import Work from './components/Work'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
