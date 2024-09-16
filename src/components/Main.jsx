694c59cf5ca7f28d391e83b0dfb3786e8d7c6abb
benzakielarz <bjzakielarz@loyola.edu>
2024-09-16 19:37:15 -0400

    updated landing screen

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
       />
       <div className="w-full h-screen absolute top-0 left-0 ">
         <div className="max-w-[700px] m-auto pt-[28%] h-[75%] flex flex-col justify-center lg:items-start items-center">
-          <h1 className="sm: text-5xl font-bold text-gray-100">I'm Benjamin Zakielarz</h1>
+          <h1 className="sm: text-5xl font-bold text-gray-100">I'm Ben Zakielarz</h1>
           <h2 className="flex sm: text-3xl pt-4 text-gray-100">
             I'm a 
             <TypeAnimation
