9bde6152871d9ac740ca7414a46f130f15c72b58
benzakielarz <bjzakielarz@loyola.edu>
2024-11-29 13:30:53 -0500

    Updated work descriptions

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -28,14 +28,14 @@ const data = [
         title: "Full Time Student",
         duration: "",
         details:
-            "I am a full-time computer science student at Loyola University Maryland, aiming to broaden my knowledge in the field and establish valuable career connections. " 
+            "I am a full-time computer science student at Pennsylvania State University, aiming to broaden my knowledge in the field and establish valuable career connections. " 
     },
     {
         year: 2024 + " - Present",
-        title: "AI Engineer",
+        title: "AI Code Contributor",
         duration: "",
         details:
-            "Tasked with training LLM's by writing and analyzing complex code, primarily in the Python and Javascript languages. I would initialize a prompt along with providing code, and ask the LLM to perform some function. The model would then return two responses, and I would pick the higher-quality response. This is my first highly paid position in the software development field." 
+            "Tasked with training LLM's by writing and analyzing complex code primarily in the Python, Java, and Javascript languages. I initialize a prompt along with providing code, and ask the LLM to perform some function. The model would then return two responses, and I would pick the higher-quality response. In most cases, I would have to rewrite the AI generated response with corrected code." 
     },
 ];
 const Work = () => {
